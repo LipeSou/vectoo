1b8596dfeaa3d3581954750c5aefa3c792e107f6
LipeSou <felipesoutomdt@gmail.com>
2023-09-14 22:36:59 -0300

    chore: config rules eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
 export default function Home() {
   return (
-    <main >
-      <div >
-      home
+    <main>
+      <div>
+        home
       </div>
     </main>
-  )
+  );
 }
