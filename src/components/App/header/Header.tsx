503a931eaf1e1196a439f17c06d17752fff66bba
LipeSou <felipesoutomdt@gmail.com>
2023-09-24 20:54:06 -0300

    fix: fix menu and screen

--- src/components/App/header/Header.tsx
+++ src/components/App/header/Header.tsx
@@ -1,8 +1,12 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import styles from './header.module.css';
-import NavBarMenu from './navbarMenu/NavBarMenu';
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
     <header className={styles.container}>
 
@@ -16,7 +20,28 @@ export default function Header() {
           <div className={styles.menuButton} />
 
           {/* navbar menu */}
-          <NavBarMenu />
+          <ul className={styles.navbar}>
+            <li className={styles.navbarLiMenu}>
+              <Link href="/" className={pathname ? styles.linkActive : styles.link}>Home</Link>
+            </li>
+            <li className={styles.navbarLiMenu}>
+              <Link href="/about" className={styles.link}>About</Link>
+            </li>
+            <li className={styles.navbarLiMenu}>
+              <Link href="/images" className={styles.link}>Get Images</Link>
+            </li>
+
+            {/* checkbox change theme */}
+            <div className="themeContainer">
+              <label htmlFor="theme" className="themeSwitch">
+                <p className={styles.mobileChangeTheme}>Theme</p>
+                <div className="themeSwitchWrapper">
+                  <input id="theme" type="checkbox" />
+                  <div className="themeSwitchButton" />
+                </div>
+              </label>
+            </div>
+          </ul>
 
         </label>
       </div>
