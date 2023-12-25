b3fe9c8084c5d913e714b2d7e9ce7f5a3b974537
LipeSou <felipesoutomdt@gmail.com>
2023-12-25 17:38:39 -0300

    feat: analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Header from '@/components/header/Header';
 import Footer from '@/components/footer/Footer';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}
+        <SpeedInsights />
         <Footer />
       </body>
     </html>
